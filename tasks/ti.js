0debd79993d9673e4c869b42533831662b60c306
Paul Ryan <paul@sharpred.com>
2015-10-28 15:14:17 +0000

    basic settings for ti build

--- tasks/ti.js
+++ tasks/ti.js
@@ -12,19 +12,23 @@ module.exports = function(grunt) {
 
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
-
+    var _ = require("lodash");
     var tiObject = {
         'build' : {
-            options : {
-                "params" : [1, 2, 3],
-                "log-level" : "info"
-            }
-        },
-        create : {}
+            "options" : {
+                "--log-level" : "info",
+                "--platform" : "ios",
+                "--project-dir" : ".",
+                "--target" : "device",
+                "--pp-uuid" : "d8cf31a2-1071-4584-90dd-48991c746bf9",
+                "--developer-name" : "Paul Ryan (5SKE6V3MFC)"
+            },
+            "args" : ['--no-banner', "--no-progress-bars", "--no-prompt", "--liveview"]
+        }
     };
     //add some defaults.  Remove these from the array as you implement them correctly.
-    ['analyze', 'analyze-options', 'analyze-plugins', 'clean', 'config', 'help', 'info', 'login', 'logout', 'module', 'plugin', 'project', 'sdk', 'setup', 'status'].forEach(function(option) {
-        tiObject[option] = {};
+    ['analyze', 'analyze-options', 'analyze-plugins', 'clean', 'config', 'create', 'help', 'info', 'login', 'logout', 'module', 'plugin', 'project', 'sdk', 'setup', 'status'].forEach(function(cmd) {
+        tiObject[cmd] = {};
     });
 
     //flesh out with some proper defaults
@@ -41,13 +45,17 @@ module.exports = function(grunt) {
             spawn = require('child_process').spawn,
             task,
             done = this.async(),
-            cwd = process.cwd();
-
+            cwd = process.cwd(),
+            params = ['ti', this.target];
         switch(this.target) {
-            //deliberate fall through
-            case 'info':
-            case 'sdk':
-                task = spawn('appc', ['ti', this.target], {
+            case 'build':
+                _.each(options, function(obj, val) {
+                    params.push(val + "=" + obj);
+                });
+                _.extend(this.data.args, params);
+                grunt.log.ok(JSON.stringify(params));
+
+                task = spawn('appc', params, {
                     'cwd' : cwd
                 });
 
@@ -62,6 +70,7 @@ module.exports = function(grunt) {
                 task.stdout.on('close', function(code) {
                     done(true);
                 });
+
                 break;
             default:
                 grunt.log.ok(this.target + " not implemented.  Fork git@github.com:sharpred/grunt-appc-cli.git and submit a pull request!");
