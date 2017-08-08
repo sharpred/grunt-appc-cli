cb8788d1a4a1ad9bab3fd052e357a73f5e7b690d
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2017-08-08 17:13:25 +0100

    removed duplicate functions

--- tasks/newapp.js
+++ tasks/newapp.js
@@ -14,71 +14,9 @@ module.exports = function (grunt) {
     // creation: http://gruntjs.com/creating-tasks
     var _ = require("lodash");
     var tiObject = {};
+    var cmdfunction = require("./lib/cmdfunction");
 
     var appcSettings = grunt.config.get("appc-cli");
 
-    grunt.registerTask('newapp', 'new appc app', function (command, job) {
-        // Merge task-specific and/or target-specific options with these defaults.
-
-        var options = this.options({}),
-            spawn = require('child_process').spawn,
-            task,
-            done = this.async(),
-            cwd = process.cwd(),
-            params = ['new', command],
-            settings = appcSettings[job];
-        
-        var buildArgs = [];
-        _.each(settings.options, function (obj, val) {
-            params.push("--" + val + "=" + obj);
-        });
-        _.extend(buildArgs, params);
-        if (settings.args) {
-            settings.args.forEach(function (item) {
-                buildArgs.push(item);
-            });
-        }
-
-        task = spawn('appc', buildArgs, {
-            'cwd': cwd
-        });
-
-        task.stdout.on('data', function (data) {
-            if (data.indexOf("Project built successfully") !== -1) {
-                done(true);
-            }
-            grunt.log.ok(data);
-        });
-
-        task.stderr.on('data', function (data) {
-            var status = data.toString().substr(0, data.toString().indexOf(" "));
-            switch (status) {
-                case "[DEBUG]":
-                    grunt.log.debug(data);
-                    break;
-                case "[INFO]":
-                    grunt.log.ok(data);
-                    break;
-                case "[WARN]":
-                    grunt.log.error(data);
-                    break;
-                case "[ERROR]":
-                    grunt.log.error(data);
-                    done(false);
-                    break;
-                default:
-                    grunt.log.ok(data);
-            }
-        });
-
-        task.stderr.on('error', function (data) {
-            grunt.log.error(data);
-            done(false);
-        });
-
-        task.stdout.on('close', function (code) {
-            done(true);
-        });
-
-    });
-};
+    grunt.registerTask('newapp', 'create new appc app', cmdfunction);
+}
--- tasks/ti.js
+++ tasks/ti.js
@@ -8,77 +8,15 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Please see the Grunt documentation for more information regarding task
     // creation: http://gruntjs.com/creating-tasks
     var _ = require("lodash");
     var tiObject = {};
+    var cmdfunction = require("./lib/cmdfunction");
 
     var appcSettings = grunt.config.get("appc-cli");
 
-    grunt.registerTask('ti', 'titanium commands', function(command, job) {
-        // Merge task-specific and/or target-specific options with these defaults.
-
-        var options = this.options({}),
-            spawn = require('child_process').spawn,
-            task,
-            done = this.async(),
-            cwd = process.cwd(),
-            params = ['ti', command],
-            settings = appcSettings[job];
-
-        var buildArgs = [];
-        _.each(settings.options, function(obj, val) {
-            params.push("--"+val + "=" + obj);
-        });
-        _.extend(buildArgs, params);
-        if (settings.args) {
-            settings.args.forEach(function(item){
-                buildArgs.push(item);
-            });
-        }
-
-        task = spawn('appc', buildArgs, {
-            'cwd' : cwd
-        });
-
-        task.stdout.on('data', function(data) {
-            if(data.indexOf("Project built successfully") !== -1) {
-                done(true);
-            }
-            grunt.log.ok(data);
-        });
-
-        task.stderr.on('data', function(data) {
-            var status = data.toString().substr(0, data.toString().indexOf(" "));
-            switch (status) {
-                case "[DEBUG]":
-                    grunt.log.debug(data);
-                    break;
-                case "[INFO]":
-                    grunt.log.ok(data);
-                    break;
-                case "[WARN]":
-                    grunt.log.error(data);
-                    break;
-                case "[ERROR]":
-                    grunt.log.error(data);
-                    done(false);
-                    break;
-                default:
-                    grunt.log.ok(data);
-            }
-        });
-
-        task.stderr.on('error', function(data) {
-            grunt.log.error(data);
-            done(false);
-        });
-
-        task.stdout.on('close', function(code) {
-            done(true);
-        });
-
-    });
-};
+    grunt.registerTask('ti', 'titanium commands', cmdfunction);
+}
